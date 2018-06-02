efd3e6726f7b473f26601be0c2d2688774cfaf20
Steven Hobson-Campbell <stevhob@microsoft.com>
2018-06-02 02:50:06 -0700

    refactor: Add HotKeyHandler

---
+++ src/HotKeyHandler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using static PInvoke.User32;
+
+namespace Snappy
+{
+    public class HotKeyHandler
+    {
+        public const uint MOD_WIN = 0x0008;
+        public const uint MOD_ALT = 0x0001;
+        public const uint MOD_NOREPEAT = 0x4000;
+
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, VirtualKey vk);
+
+        private Dictionary<int, Action> callbacks = new Dictionary<int, Action>();
+
+        public HotKeyHandler((uint, VirtualKey, Action)[] registrations)
+        {
+            Task.Run(() =>
+            {
+                var id = 0;
+                foreach (var registration in registrations)
+                {
+                    var (mods, key, callback) = registration;
+
+                    RegisterHotKey(IntPtr.Zero, id, mods, key);
+                    this.callbacks.Add(id, callback);
+                    id++;
+                }
+
+                unsafe
+                {
+                    MSG msg = new MSG();
+                    while (GetMessage(&msg, IntPtr.Zero, 0, 0) != 0)
+                    {
+                        this.callbacks.TryGetValue(msg.wParam.ToInt32(), out Action callback);
+
+                        try
+                        {
+                            callback();
+                        }
+                        catch (Exception ex)
+                        {
+                            Debug.WriteLine(ex);
+                        }
+                    }
+                }
+            });
+        }
+
+    }
+}
--- src/MainApplicationContext.cs
+++ src/MainApplicationContext.cs
@@ -2,8 +2,6 @@
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using Snappy.Properties;
 using static PInvoke.User32;
@@ -16,16 +14,10 @@ namespace Snappy
         {
             "SearchUI"
         };
-
-        private const uint MOD_WIN = 0x0008;
-        private const uint MOD_ALT = 0x0001;
-        private const uint MOD_NOREPEAT = 0x4000;
         
         private NotifyIcon trayIcon;
+        private HotKeyHandler hotKeyHandler;
 
-        [DllImport("user32.dll")]
-        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, VirtualKey vk);
-        
         private enum Direction
         {
             Up,
@@ -44,65 +36,61 @@ namespace Snappy
                 })
             };
 
-            Task.Run(() =>
+            var mods = HotKeyHandler.MOD_WIN | HotKeyHandler.MOD_ALT | HotKeyHandler.MOD_NOREPEAT;
+            this.hotKeyHandler = new HotKeyHandler(new (uint, VirtualKey, Action)[]
             {
-                RegisterHotKey(IntPtr.Zero, 0, MOD_WIN | MOD_ALT | MOD_NOREPEAT, VirtualKey.VK_UP);
-                RegisterHotKey(IntPtr.Zero, 1, MOD_WIN | MOD_ALT | MOD_NOREPEAT, VirtualKey.VK_DOWN);
-
-                unsafe
-                {
-                    MSG msg = new MSG();
-                    while (GetMessage(&msg, IntPtr.Zero, 0, 0) != 0)
-                    {
-                        var window = GetForegroundWindow();
-                        WINDOWINFO info = new WINDOWINFO();
-                        var direction = msg.wParam.ToInt32() == 0x0 ? Direction.Up : Direction.Down;
-                        if (window != null && GetWindowInfo(window, ref info))
-                        {
-                            GetWindowThreadProcessId(window, out int procId);
-                            var proc = Process.GetProcessById(procId);
-
-                            Debug.WriteLine($"{nameof(proc.ProcessName)}: {proc.ProcessName}");
-
-                            // Don't resize windows from protected processes
-                            if (MainApplicationContext.protectedProcesses.Contains(proc.ProcessName))
-                            {
-                                continue;
-                            }
-
-                            var area = Screen.GetWorkingArea(new Point(info.rcClient.left, info.rcClient.top));
-                            var leftBorder = info.rcClient.left - info.rcWindow.left;
-                            var topBorder = info.rcClient.top - info.rcWindow.top;
-                            var rightBorder = info.rcWindow.right - info.rcClient.right;
-                            var bottomBorder = info.rcWindow.bottom - info.rcClient.bottom;
-
-                            ShowWindow(window, WindowShowStyle.SW_RESTORE);
-                            if (direction == Direction.Up)
-                            {
-                                SetWindowPos(
-                                    window,
-                                    IntPtr.Zero,
-                                    area.Left - leftBorder,
-                                    area.Top - topBorder,
-                                    area.Width + leftBorder + rightBorder,
-                                    (area.Height / 2) + topBorder + bottomBorder,
-                                    SetWindowPosFlags.SWP_SHOWWINDOW);
-                            }
-                            else
-                            {
-                                SetWindowPos(
-                                    window,
-                                    IntPtr.Zero,
-                                    area.Left - leftBorder,
-                                    area.Bottom - (area.Height / 2) - topBorder,
-                                    area.Width + leftBorder + rightBorder,
-                                    (area.Height / 2) + topBorder + bottomBorder,
-                                    SetWindowPosFlags.SWP_SHOWWINDOW);
-                            }
-                        }
-                    }
-                }
+                ( mods, VirtualKey.VK_UP, () => this.ResizeWindow(Direction.Up) ),
+                ( mods, VirtualKey.VK_DOWN, () => this.ResizeWindow(Direction.Down) )
             });
+        }
+
+        private void ResizeWindow(Direction direction)
+        {
+            var window = GetForegroundWindow();
+            WINDOWINFO info = new WINDOWINFO();
+            if (window != null && GetWindowInfo(window, ref info))
+            {
+                GetWindowThreadProcessId(window, out int procId);
+                var proc = Process.GetProcessById(procId);
+
+                Debug.WriteLine($"{nameof(proc.ProcessName)}: {proc.ProcessName}");
+
+                // Don't resize windows from protected processes
+                if (MainApplicationContext.protectedProcesses.Contains(proc.ProcessName))
+                {
+                    return;
+                }
+
+                var area = Screen.GetWorkingArea(new Point(info.rcClient.left, info.rcClient.top));
+                var leftBorder = info.rcClient.left - info.rcWindow.left;
+                var topBorder = info.rcClient.top - info.rcWindow.top;
+                var rightBorder = info.rcWindow.right - info.rcClient.right;
+                var bottomBorder = info.rcWindow.bottom - info.rcClient.bottom;
+
+                ShowWindow(window, WindowShowStyle.SW_RESTORE);
+                if (direction == Direction.Up)
+                {
+                    SetWindowPos(
+                        window,
+                        IntPtr.Zero,
+                        area.Left - leftBorder,
+                        area.Top - topBorder,
+                        area.Width + leftBorder + rightBorder,
+                        (area.Height / 2) + topBorder + bottomBorder,
+                        SetWindowPosFlags.SWP_SHOWWINDOW);
+                }
+                else
+                {
+                    SetWindowPos(
+                        window,
+                        IntPtr.Zero,
+                        area.Left - leftBorder,
+                        area.Bottom - (area.Height / 2) - topBorder,
+                        area.Width + leftBorder + rightBorder,
+                        (area.Height / 2) + topBorder + bottomBorder,
+                        SetWindowPosFlags.SWP_SHOWWINDOW);
+                }
+            }
         }
 
         private void Quit(object sender, EventArgs e)
