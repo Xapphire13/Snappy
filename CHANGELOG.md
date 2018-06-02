9ac4d318bb8859dfac0260777ce3be9f09181db9
Steven Hobson-Campbell <stevhob@microsoft.com>
2018-06-01 22:43:11 -0700

    tools: Add release script

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,3 @@
-# Change Log
-
-All notable changes to this project will be documented in this file.
-See [Conventional Commits](https://conventionalcommits.org) for commit guidelines.
-
 <a name="1.0.0"></a>
 # [1.0.0](https://github.com/Xapphire13/Snappy/compare/3d3ef79...v1.0.0) (2018-06-01)
 
---
+++ scripts/release.ps1
@@ -0,0 +1,28 @@
+$ErrorActionPreference = "Stop"
+$ASSEMBLY_INFO_PATH = Resolve-Path src\Properties\AssemblyInfo.cs
+
+if (@($(git tag -l --points-at HEAD)).Contains("latest")) {
+  exit # No changes
+}
+
+$currentVersion = $(Select-String -Pattern "AssemblyVersion\(`"(.+)`"\)" -Path $ASSEMBLY_INFO_PATH).Matches.Groups[1].Value.Split(".")
+$bump = conventional-recommended-bump -p angular
+
+switch ($bump) {
+  "major" { $currentVersion[0] = 1 + $currentVersion[0] }
+  "minor" { $currentVersion[1] = 1 + $currentVersion[1] }
+  "patch" { $currentVersion[2] = 1 + $currentVersion[2] }
+}
+
+$newVersion = $currentVersion -join "."
+(Get-Content $ASSEMBLY_INFO_PATH) -replace "AssemblyVersion\(.+\)","AssemblyVersion(`"$newVersion`")" | Set-Content -Encoding UTF8 -Path $ASSEMBLY_INFO_PATH
+(Get-Content $ASSEMBLY_INFO_PATH) -replace "AssemblyFileVersion\(.+\)","AssemblyFileVersion(`"$newVersion`")" | Set-Content -Encoding UTF8 -Path $ASSEMBLY_INFO_PATH
+
+"{`"version`": `"$newVersion`"}" | Set-Content package.json
+
+standard-changelog
+git add :/
+git commit -m "release: v$newVersion"
+git tag --force "v$newVersion"
+git tag --force latest
+Remove-Item package.json
